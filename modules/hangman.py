3ae8872dc3f0997a8eba5f89484d575eaa72a11f
Doonguin <penguinicesp@gmail.com>
2024-09-12 10:22:05 +0200

    started hangman development

--- modules/hangman.py
+++ modules/hangman.py
@@ -2,9 +2,28 @@
 import os
 import random
 
+# Public variables
+words = [
+    "python", "programmeren", "utrecht",
+    "hogeschool", "opleiding", "stephan"
+]
+chosenWord = words[random.randint(0, 5)]
+dashes = []
+
+# Create the dashed according to the length of the word
+for c in chosenWord:
+    dashes += "_"
+
 # Functions
 def hangman():
-    print('Dit is hangman')
-    input('Press enter to close')
+    guessed = False
+
+    while guessed == False:
+        print(chosenWord)
+        print(dashes)
+
+        inp = input("Raad letter: ")
+    
+    
 
     return False
